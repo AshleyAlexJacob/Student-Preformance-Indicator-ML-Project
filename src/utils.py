95311f7444cd3ad4979d6ea278e0d2989800ab87
AshleyAlexJacob <alexjacob260@gmail.com>
2023-04-27 01:06:57 +0500

    👌 IMPROVE: hyperparameter tuning added

--- src/utils.py
+++ src/utils.py
@@ -15,6 +15,7 @@ from src.exception import CustomException
 import pandas as pd
 import numpy as np
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 
 def save_object(file_path:str,obj):
@@ -36,6 +37,31 @@ def evaluate_model(X_train,y_train,models:dict,X_test,y_test)-> dict:
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            model.fit(X_train,y_train)
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+            train_model_score = r2_score(y_train,y_train_pred)
+            test_model_score = r2_score(y_test,y_test_pred)
+            report[list(models.keys())[i]] = test_model_score
+        
+        return report
+
+    except Exception as e:
+        raise CustomException(e,sys)
+
+def evaluate_model_hyperparameter_tuning(X_train,y_train,models:dict,X_test,y_test, params: dict)-> dict:
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para=params[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+
             model.fit(X_train,y_train)
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
